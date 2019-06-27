89caeb8f589e52de6bb04daccaef5750ef4b3b2e
malkaviano <rafael.kss@gmail.com>
2019-06-27 15:20:36 -0300

    removed dist folder

--- dist/src/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sac_1 = require("./sac");
-var sac = new sac_1.SAC(110500, 0.6, 360);
-sac.tabela().forEach(function (e) {
-    console.log(e);
-});
-//# sourceMappingURL=app.js.map
